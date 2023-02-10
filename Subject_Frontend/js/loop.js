86457c82332066f8865fd628b0b9f26a387e7db9
AlinaRusuAW <74514427+AlinaRusuAW@users.noreply.github.com>
2023-02-10 15:45:10 +0200

    Add files via upload

--- Subject_Frontend/js/loop.js
+++ Subject_Frontend/js/loop.js
@@ -1,7 +1,9 @@
-let a = 6;
+let a = 5;
 let b = 30;
 let rez = 0;
-for(let i=a; i<=b; i=i+2){
-   rez += i;
+for(let i=a; i<=b; i++){
+   if(i % 2 == 0){
+      rez += i;
+   }
 }
    console.log("Suma numerelor pare din intervalul indicat este: " + rez);
